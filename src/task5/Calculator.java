e2406972aa8ebffacfd89b671ac83c07fa36dfb9
Volodymyr <volodymyrostash13@gmail.com>
2022-08-25 15:33:38 +0200

    new

--- src/task5/Calculator.java
+++ src/task5/Calculator.java
@@ -1,16 +1,32 @@
 package task5;
 
-public class Calculator {
-    double a;
-    double b;
+import javax.naming.NameNotFoundException;
 
-    private double add() {
+public class Calculator {
+    int a;
+    int b;
+
+    int res;
+
+    String arithmeticOperation;
+
+    public int add() {
         return a + b;
     }
-    private double sub() {
+    public int sub() {
         return a - b;
     }
-    private double mul() {
+    public int mul() {
         return a * b;
+    }
+
+    public int div() {
+        try {
+            res = (a / b);
+            System.out.println(res);
+        } catch(ArithmeticException e) {
+            System.out.println("На нуль ділити не можна");
+        }
+        return res = 0;
     }
 }
